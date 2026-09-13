5eb8f086094b87b96e27ed9a5d9fef16e93d8ee7
Shoeb Akhtar <malikshoeb924@gmail.com>
2026-09-13 16:04:14 +0530

    Add files via upload

---
+++ product.c
@@ -0,0 +1,15 @@
+# include <stdio.h>
+int main () {
+
+    int a1,a2,sum;
+
+    printf ("enter a1:");
+    scanf ("%d",& a1);
+    printf ("enter a2:");
+    scanf ("%d",& a2);
+
+    sum = a1*a2;
+
+    printf ("product = %d",sum);
+    return 0;
+}
