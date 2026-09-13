f12f6ee0ed17c6759113a70c77a4ee854c45cd98
Shoeb Akhtar <malikshoeb924@gmail.com>
2026-09-13 16:04:29 +0530

    Add files via upload

---
+++ sum.c
@@ -0,0 +1,15 @@
+# include <stdio.h>
+int main () {
+    int n1,n2,sum;
+
+    printf ("enter n1:");
+    scanf ("%d",& n1);
+    printf ("enter n2:");
+    scanf ("%d",& n2);
+
+    sum = n1+n2;
+
+    printf ("sum = %d",sum);
+    return 0;
+
+}
