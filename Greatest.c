ab0af74cc98da61c469289d2d390bbd5ead40757
Shoeb Akhtar <malikshoeb924@gmail.com>
2026-09-20 01:20:26 +0530

    Add files via upload

---
+++ Greatest.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    if (a > b)
+        printf("%d is the greatest number", a);
+    else if (b > a)
+        printf("%d is the greatest number", b);
+    else
+        printf("Both numbers are equal");
+
+    return 0;
+}
