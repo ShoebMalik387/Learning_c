fa290c1ad7ff3319081f034013099b70cb034632
Shoeb Akhtar <malikshoeb924@gmail.com>
2026-09-21 01:13:04 +0530

    Add files via upload

---
+++ Switch.c
@@ -0,0 +1,52 @@
+# include <stdio.h>
+int main () {
+
+    int day;
+
+    printf ("Enter The Day:");
+    scanf ("%d",& day);
+
+
+    switch (day) {
+
+        case 1:
+
+           printf ("Sunday");
+           break;
+
+        case 2:
+
+           printf ("Monday");
+           break;
+
+        case 3:
+
+           printf ("Tuesday");
+           break;
+
+        case 4:
+
+           printf ("Wednesday");
+           break;
+
+        case 5:
+
+           printf ("Thrusday");
+           break;
+
+        case 6:
+
+          printf ("Friday");
+          break;
+
+        case 7:
+
+          printf ("Saturday");
+          break;
+
+
+
+    }
+
+    return 0;
+} 
