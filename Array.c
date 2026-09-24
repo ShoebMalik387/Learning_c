c05cb5811ed2771895839cf7bd9fd90cfa0b560b
Shoeb Akhtar <malikshoeb924@gmail.com>
2026-09-25 00:25:26 +0530

    Add files via upload

---
+++ Array.c
@@ -0,0 +1,11 @@
+# include <stdio.h>
+int main () {
+
+    int marks [5] = {54,76,63,95,62};
+    int i;
+
+    for (i = 0; i < 5; i ++) {
+        printf ("%d\n",marks [i]);
+    }
+    return 0;
+}
